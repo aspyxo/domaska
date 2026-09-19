1184c17b5383dcadbb999e423361a3b056349584
aspyxo <ttoottoo156@gmail.com>
2026-09-19 21:40:08 +0500

    Add files via upload

---
+++ goland/ch1/server2/main.go
@@ -0,0 +1,33 @@
+// минимальный "echo"-сервер со счётчиком запросов.
+package main
+
+import (
+	"fmt"
+	"log"
+	"net/http"
+	"sync"
+)
+
+var mu sync.Mutex
+var count int
+
+func main() {
+	http.HandleFunc("/", handler)
+	http.HandleFunc("/count", counter)
+	log.Fatal(http.ListenAndServe("localhost:8000", nil))
+}
+
+// handler возвращает компонент пути запрашиваемого URL.
+func handler(w http.ResponseWriter, r *http.Request) {
+	mu.Lock()
+	count++
+	mu.Unlock()
+	fmt.Fprintf(w, "URL.Path = %q\n", r.URL.Path)
+}
+
+// counter возвращает количество сделанных запросов.
+func counter(w http.ResponseWriter, r *http.Request) {
+	mu.Lock()
+	fmt.Fprintf(w, "Count %d\n", count)
+	mu.Unlock()
+}
